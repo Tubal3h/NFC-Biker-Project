49c92ab924182454af4f1a7523e7b1a5e7f3ef7c
Macwell <2bal3h@gmail.com>
2025-08-18 14:50:53 +0200

    fix: Risolve conflitto di merge e imposta URL di produzione finale

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
 export const environment = {
   production: true,
   apiUrl: 'https://sos-helmet-backend.onrender.com/api' // <-- METTI QUI IL TUO URL DI RENDER
 };
-=======
-/* -------------------------------------------------------------------------- */
-/*                                SERVER ONLINE                               */
-/* -------------------------------------------------------------------------- */
-
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.soshelmet.it/api' // <-- METTI QUI IL TUO URL DI RENDER
-};
-
-
-/* -------------------------------------------------------------------------- */
-/*                                SERVER LOCALE                               */
-/* -------------------------------------------------------------------------- */
-
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:3000/api' // <-- Deve avere /api anche qui
-// };
->>>>>>> 6bdc15b4bd4e9b4fd9af89e3caa2e4e7b77f8a94
