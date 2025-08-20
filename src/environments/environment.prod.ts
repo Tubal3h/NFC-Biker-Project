33fcb949ca5113e1008cc98ffb4f14e7c28fbe46
Macwell <2bal3h@gmail.com>
2025-08-20 20:14:42 +0200

    fix api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,10 @@
 //   production: true,
 //   apiUrl: 'https://api.soshelmet.it/api' // <-- METTI QUI IL TUO URL DI RENDER
 // };
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://sos-helmet-api.fly.dev/api' // <-- METTI QUI IL TUO URL DI RENDER
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://sos-helmet-api.fly.dev/api' // <-- METTI QUI IL TUO URL DI RENDER
+};
 
 
 
@@ -16,7 +16,7 @@
 /*                                SERVER LOCALE                               */
 /* -------------------------------------------------------------------------- */
 
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api' // <-- Deve avere /api anche qui
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api' // <-- Deve avere /api anche qui
+// };
