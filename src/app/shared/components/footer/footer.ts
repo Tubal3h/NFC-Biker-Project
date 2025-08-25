6713a532e384689f892a44c9f37364e68f023fc8
Macwell <2bal3h@gmail.com>
2025-08-25 15:57:00 +0200

    fix ux

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -2,15 +2,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTelegramPlane, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
   // Aggiunge l'anno corrente per il copyright dinamico
   currentYear = new Date().getFullYear();
+
+  // Icona di Telegram
+  faTelegramPlane = faTelegramPlane;
+  faInstagram = faInstagram;
+  faWhatsapp = faWhatsapp;
 }
